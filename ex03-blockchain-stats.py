1a707f6838be45481e1ff4bb894db9b3a61a24dc
Jamiel Sheikh <i@jamiel.net>
2018-03-19 18:34:46 -0400

    tweaks

--- ex03-blockchain-stats.py
+++ ex03-blockchain-stats.py
@@ -1,7 +1,19 @@
-
-# Blockchain.info Block Exploration
-
-
+# Copyright Chainhaus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Block Exploration
+#
 # Docs: https://github.com/blockchain/api-v1-client-python/blob/master/docs/statistics.md
 
 from blockchain import statistics as statistics
