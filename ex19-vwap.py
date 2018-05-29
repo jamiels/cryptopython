0285ce9c60b150e457662140d670be57d2ecf424
Jamiel Sheikh <i@jamiel.net>
2018-05-29 01:53:23 -0400

    Update ex19-vwap.py

--- ex19-vwap.py
+++ ex19-vwap.py
@@ -17,68 +17,77 @@
 # Docs: https://docs.gdax.com
 
 
-import requests
-import matplotlib.pyplot as plt
+import requests, io, time
 import pandas as pd
-import json
-import io
-import time
 import datetime as dt
 
+
 def main():    
+    pairs = ['eth-usd','btc-usd']
 
     blotter = initialize_blotter()
-    pairs = ['ethusd','btcusd'] # assume usd base
     pl = initialize_pl(pairs)
-
-    data = pd.DataFrame([[dt.datetime.now(),'ETH',1.223,541.33]] ,columns=['Timestamp','Pair','Volume','Executed Price'])
-    blotter = blotter.append(data, ignore_index=True)
-    pl = update_pl(pl,'ethusd',1.223,541.33)
-    print(pl)
-
-    data = pd.DataFrame([[dt.datetime.now(),'ETH',2.623,561.33]] ,columns=['Timestamp','Pair','Volume','Executed Price'])
-    blotter = blotter.append(data, ignore_index=True)
-    pl = update_pl(pl,'ethusd',2.623,561.33)
-    print(pl)
-
-
-    data = pd.DataFrame([[dt.datetime.now(),'ETH',2.723,571.33]] ,columns=['Timestamp','Pair','Volume','Executed Price'])
-    blotter = blotter.append(data, ignore_index=True)
     
-    data = pd.DataFrame([[dt.datetime.now(),'ETH',3.43, 521.33]] ,columns=['Timestamp','Pair','Volume','Executed Price'])
+    blotter = trade(blotter,1,pairs[0])
+    blotter = trade(blotter,2,pairs[0])
+    blotter = trade(blotter,-1,pairs[0])
+
+    print(blotter)
+
+
+def trade(blotter,qty,pair):
+    bid, ask = get_price(pair)
+    data = pd.DataFrame([[dt.datetime.now(), pair ,qty, ask]] ,columns=['Timestamp','Pair','Quantity','Executed Price'])
     blotter = blotter.append(data, ignore_index=True)
+    return blotter
 
 def calc_vwap(current_qty,current_vwap,qty,price):
-    dollar = current_qty * current_vwap
-    new_dollar = dollar + (qty * price)
+    current_dollar = current_qty * current_vwap
+    new_dollar = current_dollar + (qty * price)
     new_qty = current_qty + qty
     new_vwap = new_dollar / new_qty
     return new_vwap
-
-def update_pl(pl,pair,qty,price):
-    if qty > 0: # buy
-        current_qty = pl.at[pair,'Position']
-        current_vwap = pl.at[pair,'VWAP']
-        new_vwap = calc_vwap(current_qty,current_vwap,qty,price)
-        pl.at[pair,'Position'] = current_qty + qty
-        pl.at[pair,'VWAP'] = new_vwap
-
-    return pl
-
-
-def initialize_blotter():
-    col_names = ['Timestamp','Pair','Quantity','Executed Price']
-    return pd.DataFrame(columns=col_names)
-
-
+    
+# Inititalize PL
 def initialize_pl(pairs):
     col_names = ['Pairs','Position','VWAP','UPL','RPL']
     pl = pd.DataFrame(columns=col_names)
     for p in pairs:
         data = pd.DataFrame([[p,0,0,0,0]] ,columns=col_names)
         pl = pl.append(data, ignore_index=True)
-    pl = pl.set_index('Pairs')
     return pl
+
+# Get current pair price
+def get_price(pair):
+    df = load('https://api.gdax.com/products/'+pair+'/book',printout=False)
+    ask = df.iloc[0]['asks'][0]
+    bid = df.iloc[0]['bids'][0]
+    return float(bid), float(ask)
+
+
+# Initialize a new blotter  
+def initialize_blotter():
+    col_names = ['Timestamp','Pair','Quantity','Executed Price']
+    return pd.DataFrame(columns=col_names)
+
+
+
+
+# Load function
+def load(url,printout=False,delay=0,remove_bottom_rows=0,remove_columns=[]):
+    time.sleep(delay)
+    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+    r = requests.get(url, headers=header)
+    df = pd.read_json(r.text)
+
+    if remove_bottom_rows > 0:
+        df.drop(df.tail(remove_bottom_rows).index,inplace=True)
+    df.drop(columns=remove_columns,axis=1)
+    df = df.dropna(axis=1)
+    if printout:
+        print(df)
+    return df
+
 
 if __name__ == "__main__":
     main()
