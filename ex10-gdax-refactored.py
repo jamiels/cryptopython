d9530ea671a84c2469e4999ab6c1963f55c2a800
Jamiel Sheikh <i@jamiel.net>
2018-05-29 00:20:39 -0400

    Update ex10-gdax-refactored.py

--- ex10-gdax-refactored.py
+++ ex10-gdax-refactored.py
@@ -16,24 +16,24 @@
 #
 # Docs: https://docs.gdax.com/
 
-import requests
+import requests, io, time
 import matplotlib.pyplot as plt
 import pandas as pd
-import json
-import io
-import time
 
 def main():
-    load('http://api.coindesk.com/v1/bpi/historical/close.json?start=2018-01-01&end=2018-03-18',remove_bottom_rows=2,remove_columns=['disclaimer'])
+    df = load('http://api.coindesk.com/v1/bpi/historical/close.json?start=2018-05-10&end=2018-05-18',remove_bottom_rows=2,remove_columns=['disclaimer'])
+    print(df)
 
 def load(url,printout=False,delay=0,remove_bottom_rows=0,remove_columns=[]):
     time.sleep(delay)
     header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
     r = requests.get(url, headers=header)
     df = pd.read_json(r.text)
+
+
     if remove_bottom_rows > 0:
         df.drop(df.tail(remove_bottom_rows).index,inplace=True)
-    df.drop(columns=remove_columns,axis=1)
+    df.drop(columns=remove_columns,axis=1,inplace=True)
     df = df.dropna(axis=1)
     if printout:
         print(df)
