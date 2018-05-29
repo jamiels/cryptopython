0c7e5a5b4f552b6a0406cf255f51d7f8cefe0f6c
Jamiel Sheikh <i@jamiel.net>
2018-05-29 00:09:48 -0400

    Update and rename ex09-gdax.py to ex09-coindesk.py

--- ex09-coindesk.py
+++ ex09-coindesk.py
@@ -12,26 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# GDAX
 #
-# Docs: https://docs.gdax.com/
+# Docs: https://www.coindesk.com/api/
 
-
-import requests
+import requests, io
 import matplotlib.pyplot as plt
 import pandas as pd
-import json
-import io
 
 
-import pandas as pd
-import requests
 
 def main():
-    url = 'http://api.coindesk.com/v1/bpi/historical/close.json?start=2018-01-01&end=2018-03-18'
+    url = 'http://api.coindesk.com/v1/bpi/historical/close.json?start=2018-01-01&end=2018-05-28'
     header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
     r = requests.get(url, headers=header)
     df = pd.read_json(r.text)
+    print(df.columns)
+    print(df.shape)
+    print(df.head())
+
+    # Get bpi column
     df = df['bpi']
     df.drop(df.tail(2).index,inplace=True)
     print(df)
